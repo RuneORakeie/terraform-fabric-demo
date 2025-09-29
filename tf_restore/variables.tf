1dfe8d042b0b1c12e1118b70a09190753adfdba5
Rune Ovlien Rakeie <rune.rakeie@tietoevry.com>
2025-09-29 15:53:51 +0200

    Add: restore fabric items

--- tf_restore/variables.tf
+++ tf_restore/variables.tf
@@ -21,7 +21,7 @@ variable "client_secret" {
   description = "Azure client secret"
   type        = string
   sensitive   = true
-  # Will read from TF_VAR_client_secret environment variable
+  # Will read from TF_VAR_arm_client_secret environment variable
 }
 # variable "local_dev_client_id" {
 #   description = "Azure client ID"
